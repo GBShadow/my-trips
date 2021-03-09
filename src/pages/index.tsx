f1fd5823be47c1dafd631ec90d8875cd3d3fa678
GBShadow <gbs.shadow@gmail.com>
2021-03-09 17:28:41 -0300

    Pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,23 @@
-import { InfoOutline } from '@styled-icons/evaicons-outline';
-import LinkWrapper from 'components/LinkWrapper';
-import dynamic from 'next/dynamic';
+import { MapProps } from "components/Map";
+import client from "graphql/client";
+import { GetPlacesQuery } from "graphql/generated/graphql";
+import { GET_PLACES } from "graphql/queries";
+import { GetStaticProps } from "next";
+import HomeTemplate from "templates/Home";
 
-const Map = dynamic(() => import('components/Map'), { ssr: false });
-
-export default function Home() {
+export default function Home({ places }: MapProps) {
   return (
-    <>
-      <LinkWrapper href="/about">
-        <InfoOutline size={32} aria-label="About" />
-      </LinkWrapper>
-      <Map />
-    </>
+   <HomeTemplate places={places} />
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { places } = await client.request<GetPlacesQuery>(GET_PLACES, { first: 10 });
+
+  return {
+    props: {
+      places
+    },
+  };
+};
+
